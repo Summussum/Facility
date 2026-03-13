014cb949f39f9bcb3a3f1262b9c0a15f1de2fb8e
Summussum <summussum9209@gmail.com>
2026-03-13 10:50:35 -0700

    add Tasks Done Today

--- todo/utility.py
+++ todo/utility.py
@@ -10,6 +10,7 @@ def get_task_groups(task_data):
     custom = task_data.filter(schedule_type="custom", deadline__isnull=False).order_by('deadline')
     overdue = task_data.filter(deadline__lt=date.today(), deadline__isnull=False).exclude(schedule_type="daily").order_by('deadline')
     upcoming = task_data.filter(deadline__gte = date.today(), deadline__isnull=False).exclude(schedule_type="daily").order_by('deadline')
+    today = task_data.filter(previous__date = date.today())
     paused = task_data.filter(deadline__isnull=True)
     task_groups = {
         "daily": daily,
@@ -20,6 +21,7 @@ def get_task_groups(task_data):
         "custom": custom,
         "overdue": overdue,
         "upcoming": upcoming,
+        "today": today,
         "paused": paused
     }
     return task_groups
